614d2cb1c2473d6d55136236bd40c270f6ebf841
ik_2dw3 <ik_2dw3@H015110.NET>
2020-01-07 09:10:37 +0100

    sesion fin

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+    //Rutas para logearte
+    Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+    // Ruta del inicio
+    Route::get('/', function () {
+        return view('welcome');        
+    });
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/peticiones', 'PeticionesController@peticion');
+    Route::get('/home', 'HomeController@getIndex');
+    
+
+    // Rutas para imprimir cartas
+    Route::get('/imprimirInformacion', 'ImprimirController@getInformacion');
+    Route::get('/imprimirPreguntas', 'ImprimirController@getPreguntas');
+    Route::get('/imprimirTablero', 'ImprimirController@getTablero');
+
+
+    // Rutas para las peticiones
+    Route::get('/peticionMujeres', 'PeticionController@getFormulario');
+    Route::get('/peticiones', 'PeticionController@getTabla');
+
+
+?>
+
